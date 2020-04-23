6e10d72e457a9898e4d37e1b3b095b085dd50cd2
Enrico <keridano@gmail.com>
2020-04-23 22:57:11 +0200

    Adde prefabs, fixed scripts, tidied up

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,21 +1,9 @@
 ﻿using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour
 {
     void Awake()
     {
         DontDestroyOnLoad(gameObject);    
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Invoke("LoadFirstScene", 3f);
-    }
-
-    private void LoadFirstScene()
-    {
-        SceneManager.LoadScene(1);
     }
 }
