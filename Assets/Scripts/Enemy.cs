87794df1ec00858f4aba5b4f56cfc1662934b3e9
Enrico <keridano@gmail.com>
2020-04-25 14:58:32 +0200

    Added explosion instantiated programmatically in enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,8 +2,17 @@
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathFx;
+
+    void Start()
+    {
+        var boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
     private void OnParticleCollision(GameObject other)
     {
+        Instantiate(deathFx, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 }
